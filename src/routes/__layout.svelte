2b588ccf6a372301602f127da7cbe270d6454328
harryli0088 <harryli0088@gmail.com>
2021-11-10 21:12:31 -0500

    working select image to analyze

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  //https://kit.svelte.dev/docs#layouts
 	import '../app.css';
+
+  import Blanchor from '$lib/Blanchor.svelte';
 </script>
 
 <main>
@@ -7,36 +10,29 @@
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+  <p>Built using <Blanchor href="https://kit.svelte.dev/">Svelte Kit</Blanchor></p>
+  <p>Github Repo: <Blanchor href="https://github.com/harryli0088/tfjs-test">https://github.com/harryli0088/tfjs-test</Blanchor></p>
+  <p>Icons provided by Font Awesome: <Blanchor href="https://fontawesome.com/license">https://fontawesome.com/license</Blanchor></p>
 </footer>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
+  footer {
+		padding: 1em;
+	}
+	@media only screen and (min-width: 600px) {
+		footer {
+			padding-left: var(--tablet-padding);
+			padding-right: var(--tablet-padding);
+		}
+	}
+	@media only screen and (min-width: 1000px) {
+		footer {
+			padding-left: var(--desktop-padding);
+			padding-right: var(--desktop-padding);
+		}
 	}
 
 	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
+		background-color: #ddd;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,108 +1,255 @@
 <script lang="ts">
-	/**
-   * @license
-   * Copyright 2019 Google LLC. All Rights Reserved.
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   * http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   * =============================================================================
-   */
-  import '@tensorflow/tfjs-backend-cpu';
-  import '@tensorflow/tfjs-backend-webgl';
+  import '@tensorflow/tfjs-backend-cpu'
+  import '@tensorflow/tfjs-backend-webgl'
 
-  import * as cocoSsd from '@tensorflow-models/coco-ssd';
+  import * as cocoSsd from '@tensorflow-models/coco-ssd'
 
-  import imageURL from '$lib/image1.jpg';
-  import image2URL from '$lib/image2.jpg';
-  import { onMount } from 'svelte';
+  import imageURL from '$lib/image1.jpg'
+  import image2URL from '$lib/image2.jpg'
+  import { onMount } from 'svelte'
+  import { afterUpdate } from 'svelte/internal';
+
+  //DPR is important for improving the picture quality of the canvas, especially for text
+  //based off this fiddle http://jsfiddle.net/65maD/83/ from this stack answer https://stackoverflow.com/a/54027313
 
   let canvas: HTMLCanvasElement
-  let image: HTMLImageElement
+  let canvasContainerWidth: number = 600
+  let ctx: CanvasRenderingContext2D
+  let DPR = 1
+  let images:string[] = [
+    imageURL,
+    image2URL
+  ]
   let imageSrc: string = imageURL
   let modelPromise: Promise<any>
+  let predictionTime: number = 0
+  let resultHoverIndex: number = -1
+  let results: {
+    bbox: [number,number,number,number],
+    class: string,
+    score: number,
+  }[] = []
+  let state:string = "loading"
+  let targetImage: HTMLImageElement
 
-  onMount(() => {
+  onMount(async () => {
+    DPR = window.devicePixelRatio
+    
+    ctx = canvas.getContext('2d')
+    ctx.scale(DPR, DPR)
+
     modelPromise = cocoSsd.load()
+    await run()
+    state = "loaded"
   })
 
   const onChange = async (event) => {
-    console.log("event", event)
+    state = "loading"
     const model = await modelPromise
     model.dispose()
     modelPromise = cocoSsd.load(
       {base: event.srcElement.options[event.srcElement.selectedIndex].value}
     )
+    await run()
+  }
+
+  const changeImage = async (src: string) => {
+    imageSrc = src
+    await run()
   }
 
   const run = async () => {
-    const model = await modelPromise
-    console.log("MODEL", model)
-    console.log('model loaded')
-    console.time('predict1')
-    const result = await model.detect(image)
-    console.timeEnd('predict1')
+    state = "loading"
+    const model = await modelPromise //save models as static JSON?
+    const start = Date.now()
+    results = await model.detect(targetImage)
+    predictionTime = Date.now() - start
+    console.timeEnd('prediction time')
 
-    const ctx = canvas.getContext('2d')
-    ctx.drawImage(image, 0, 0)
+    state = "loaded"
+  }
+
+  $: height = targetImage?.height || 600
+  $: width = targetImage?.width || 400
+
+  afterUpdate(() => {
+    ctx.drawImage(targetImage, 0, 0)
     ctx.font = '10px Arial'
 
-    console.log('number of detections: ', result.length)
-    for (let i = 0; i < result.length; i++) {
+    results.forEach((r,i) => {
+      const isHovering = resultHoverIndex === i
+
       ctx.beginPath()
-      ctx.rect(...result[i].bbox)
-      ctx.lineWidth = 1
+      ctx.rect(...r.bbox)
+      ctx.lineWidth = isHovering ? 3 : 1
       ctx.strokeStyle = 'green'
       ctx.fillStyle = 'green'
       ctx.stroke()
       ctx.fillText(
-        result[i].score.toFixed(3) + ' ' + result[i].class, result[i].bbox[0],
-        result[i].bbox[1] > 10 ? result[i].bbox[1] - 5 : 10
+        `${r.score.toFixed(3)} ${r.class}`,
+        r.bbox[0],
+        r.bbox[1] > 10 ? r.bbox[1] - 5 : 10
       )
-    }
-  }
-
- 
-
+    })
+  })
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
+<header>
+  <h1>TensorFlow.js Object Detection</h1>
+</header>
+
 <section>
-	<h1>TensorFlow.js Object Detection</h1>
-  <select id='base_model' on:change={onChange}>
-    <option value="lite_mobilenet_v2">SSD Lite Mobilenet V2</option>
-    <option value="mobilenet_v1">SSD Mobilenet v1</option>
-    <option value="mobilenet_v2">SSD Mobilenet v2</option>
-  </select>
-  <button type="button" id="run" on:click={run}>Run</button>
-  <button type="button" on:click={() => imageSrc = imageSrc===imageURL ? image2URL : imageURL}>Toggle Image</button>
-  <div>
-    <img id="image" src={imageSrc} alt="input" bind:this={image}/>
-    <canvas id="canvas" width="600" height="399" bind:this={canvas}/>
+  <div style="position: relative">
+    <h3>Select a Model</h3>
+    <div>
+      <select id='base_model' on:change={onChange}>
+        <option value="lite_mobilenet_v2">SSD Lite Mobilenet V2</option>
+        <option value="mobilenet_v1">SSD Mobilenet v1</option>
+        <option value="mobilenet_v2">SSD Mobilenet v2</option>
+      </select>
+    </div>
+
+
+    <div>
+      <h3>Select an Image to Analyze</h3>
+      <div id="carousel">
+        {#each images as src}
+          <img class="image-option" {src} alt="analyze option" on:click={() => changeImage(src)}/>
+        {/each}
+      </div>
+    </div>
+
+    <br/>
+
+    <div>
+      <img
+        alt="input"
+        bind:this={targetImage}
+        id="target-image"
+        src={imageSrc}
+      />
+    </div>
+
+    <div bind:clientWidth={canvasContainerWidth}>
+      <canvas
+        bind:this={canvas}
+        id="canvas"
+        height={height}
+        width={width}
+      />
+    </div>
+
+    <br/>
+
+    <div><b>Prediction Time:</b> {predictionTime}ms</div>
+
+    <br/>
+
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Class</th>
+            <th>Score</th>
+            <th>Top Left X</th>
+            <th>Top Left Y</th>
+            <th>Width</th>
+            <th>Height</th>
+          </tr>
+        </thead>
+
+        <tbody on:mouseout={() => resultHoverIndex = -1} on:blur={() => resultHoverIndex = -1}>
+          {#each results as r,i}
+            <tr on:mouseenter={() => resultHoverIndex = i}>
+              <td>{r.class}</td>
+              <td>{r.score.toFixed(3)}</td>
+              <td>{Math.round(r.bbox[0])}</td>
+              <td>{Math.round(r.bbox[1])}</td>
+              <td>{Math.round(r.bbox[2])}</td>
+              <td>{Math.round(r.bbox[3])}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+
+    {#if state === "loading"}
+      <div id="loading-dimmer">Loading...</div>
+    {/if}
   </div>
 </section>
 
 <style>
-	section {
+  header {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 1;
-	}
+		justify-content: center;
+		background-color: #17202A;
+		color: white;
+    text-align: center;
+    padding-top: 5em;
+    padding-bottom: 5em;
+  }
 
-	h1 {
-		width: 100%;
-	}
+  #target-image {
+    display: none;
+  }
 
+  canvas {
+    display: block;
+    width: 100%;
+  }
+
+  table {
+    border-collapse: collapse;
+  }
+
+  th, td {
+    text-align: left;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+  th:first-child, td:first-child {
+    padding-left: 0;
+  }
+  th:last-child, td:last-child {
+    padding-right: 0;
+  }
+
+  tbody tr {
+    transition: 0.5s;
+  }
+  tbody tr:hover {
+    background-color: #eee;
+  }
+
+  #loading-dimmer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(230,230,230,0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+  }
+
+  #carousel {
+    display: flex;
+  }
+
+  .image-option {
+    cursor: pointer;
+    height: 100px;
+    margin-right: 10px;
+  }
+  .image-option:last-child {
+    margin-right: 0;
+  }
 </style>
